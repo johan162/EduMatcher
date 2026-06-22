9865d2a870e8532b83a9578176d43e7d9743c330
Johan Persson <johan162@gmail.com>
2026-06-22 18:18:44 +0200

    Add script for counting number of Glossary in the exchange intro book

---
+++ docs-exchange-intro/scripts/count_gloss.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Count the number of glossary entries in the Glossary chapter.
+
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
+GLOSSARY_FILE="src/90-backmatter/20-glossary.md"
+NUM_GLOSSARY=`grep -n '^\*\*[A-Za-z_ /()0-9-]*:\*\*' "$GLOSSARY_FILE" | wc -l`
+
+# Strip leading whitespace from the count
+NUM_GLOSSARY=$(echo "$NUM_GLOSSARY" | sed 's/^[ \t]*//')
+
+echo -e "${WHITE}Number of glossary entries: ${YELLOW}$NUM_GLOSSARY${NC}"
+
+
